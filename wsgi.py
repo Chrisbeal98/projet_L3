7c9eed4158ef6be8d1317da5ce17bd387a506ea2
chris98 <kouadiochrisherve@gmail.com>
2026-07-05 11:53:15 +0000

    Ajout route temporaire make-admin

--- wsgi.py
+++ wsgi.py
@@ -1,2 +1,13 @@
-from app import create_app
+from app import create_app, db
+from app.models import User
+
 app = create_app()
+
+@app.route('/dev/make-admin/<email>')
+def make_admin(email):
+    user = User.query.filter_by(email=email).first()
+    if user:
+        user.role = 'admin'
+        db.session.commit()
+        return f"OK - {email} est maintenant admin"
+    return "Utilisateur pas trouvé"
